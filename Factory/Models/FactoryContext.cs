080db0b37e028739c96f13f36202b2bb5fd7aa6f
HarrisonStrand <harrisonstrand@gmail.com>
2021-01-08 08:59:48 -0800

    add Initial migration for m-m between Machine and Engineer Models

---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class FactoryContext : DbContext
+  {
+    public DbSet<Machine> Machines { get; set; }
+    public DbSet<Engineer> Engineers { get; set; }
+    public DbSet<EngineerMachine> EngineerMachine { get; set; }
+    public FactoryContext(DbContextOptions options) : base(options) { }
+  }
+}
